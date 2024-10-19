d592b007c06cdc00cfbd616bed7588ad373453eb
Vaishnav2009 <vsreekumar4u@gmail.com>
2024-10-19 14:50:12 +0100

    End of section 10

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
-import { ShopService } from './core/services/shop.service';
 import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
